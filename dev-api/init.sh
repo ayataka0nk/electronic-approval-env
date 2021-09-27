0d3aa29fc7d99e0b4257fc034ef5815983161ccf
ayataka0nk <naoto.kamba@gmail.com>
2021-09-28 03:47:55 +0900

    api開発環境整備

--- dev-api/init.sh
+++ dev-api/init.sh
@@ -2,7 +2,7 @@
 
 if [ ! -e '/workspace/electronic-approval-api' ]; then
   cd /workspace
-  git clone https://github.com/ayataka0nk/electronic-approval-api.git
+  git clone git@github.com:ayataka0nk/electronic-approval-api.git
   cd electronic-approval-api
   cp /temp/api-env .env
   npm ci
